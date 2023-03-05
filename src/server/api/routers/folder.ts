257ae179ebf9b621d330077961b6c38a028adf65
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-03-05 00:23:35 -0300

    Updated folder trcp router

--- src/server/api/routers/folder.ts
+++ src/server/api/routers/folder.ts
@@ -10,14 +10,79 @@ export const folderRouter = createTRPCRouter({
       },
     });
   }),
+  getAllTopLevel: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.folder.findMany({
+      where: {
+        parentFolderId: null,
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+  getByParentFolderId: protectedProcedure
+    .input(z.object({ parentFolderId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.folder.findMany({
+        where: {
+          parentFolderId: input.parentFolderId,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+  getById: protectedProcedure
+    .input(z.object({ folderId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.folder.findFirst({
+        where: {
+          id: input.folderId,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
   create: protectedProcedure
-    .input(z.object({ title: z.string() }))
+    .input(
+      z.object({ title: z.string(), parentFolderId: z.string().optional() })
+    )
     .mutation(({ ctx, input }) => {
       return ctx.prisma.folder.create({
         data: {
-          userId: ctx.session.user.id,
           title: input.title,
+          userId: ctx.session.user.id,
+          parentFolderId: input.parentFolderId,
         },
       });
     }),
 });
+
+/*
+Prisma doesnt have suport for a includeRecursive-like query for one to many self relating tables
+Dynamically fetch the 'depth' number of levels for subfolders property. 
+TODO Check type safety 
+TODO Adap method below to fetch the Top-Level Folders 
+TODO and only return titles and ids for tree visualisation
+? A
+getWithSubfolders: protectedProcedure
+    .input(z.object({ treeNodeId: z.string(), depth: z.number() }))
+    .query(({ ctx, input }) => {
+
+      let includeObject: any = {
+          include: {subfolders: true}
+      }
+
+      let pointer = includeObject.include;
+
+      for (let i = 0; i < depth - 1; i++) {
+          pointer.children = {include: {subfolders: true}};
+          pointer = pointer.children.include;
+      }
+
+      return ctx.prisma.folder.findUnique({
+          where: {
+              id: treeNodeId
+              // ! userId: ctx.session.user.id, 
+              // ! findUnique doesnt support non unique fields, like userId, in 'where' query
+              // ? Use firstFirst instead?
+          },
+          include: includeObject.include
+    });
+
+*/
