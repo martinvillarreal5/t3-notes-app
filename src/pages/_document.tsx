a800f58481624244cba8cfc866153de7604b3b88
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-03-22 22:21:46 -0300

    Added _document file, updated layout component

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html
+      //data-theme="night"
+      data-theme="dracula"
+      //data-theme="dark"
+      //data-theme="business"
+    >
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
