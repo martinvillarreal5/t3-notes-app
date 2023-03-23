fdc656590e4138f4bb4a52ffcdf59166fa36b971
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-03-22 22:25:54 -0300

    Added headlessUIModal

---
+++ src/components/headlessUIModal.tsx
@@ -0,0 +1,72 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+type ModalProps = {
+  isOpen: boolean;
+  setIsOpen: (value: React.SetStateAction<boolean>) => void;
+  children?: React.ReactNode;
+  title?: string;
+  description?: string;
+};
+
+export default function Modal({
+  isOpen,
+  setIsOpen,
+  title,
+  description,
+  children,
+}: ModalProps) {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => setIsOpen(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-60" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel
+                className="w-full max-w-md transform overflow-hidden 
+                rounded bg-base-100
+                p-6 text-left align-middle shadow-xl transition-all"
+              >
+                {title && (
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6"
+                  >
+                    {title}
+                  </Dialog.Title>
+                )}
+                {description && (
+                  <Dialog.Description>{description}</Dialog.Description>
+                )}
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
