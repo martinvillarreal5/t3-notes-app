a9dc100b595377cf963b1208cc0da32fcf5093c7
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-03-27 17:13:25 -0300

    Removed example

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,22 +5,11 @@ import { api } from "~/utils/api";
 
 const Login: NextPage = () => {
   const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
   return (
     <Layout>
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Sign In
-        </h1>
-      </div>
       <div className="flex flex-col items-center justify-center gap-4">
         <p className="text-center text-2xl text-white">
           {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-          {secretMessage && <span> - {secretMessage}</span>}
         </p>
         <button
           className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { folderRouter } from "~/server/api//routers/folder";
 import { noteRouter } from "./routers/note";
 
@@ -9,7 +8,6 @@ import { noteRouter } from "./routers/note";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   folder: folderRouter,
   note: noteRouter,
 });
