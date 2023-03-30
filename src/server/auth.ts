9f371f65cb74aa98297b21b86960c89ed942daa7
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-03-30 12:51:51 -0300

    Disabled Github Auth Provider for now

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,8 +5,7 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import GithubProvider from "next-auth/providers/github";
-//import GoogleProvider from "next-auth/providers/google";
+//import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -53,13 +52,9 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    GithubProvider({
+    /* GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
-    /* GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }), */
     /**
      * ...add more providers here.
