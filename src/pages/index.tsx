94f1e666a8f4176322d1d4a5a37583cdcca55563
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-01 12:30:56 -0300

    Updated home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Layout from "~/components/layout";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-  const { status } = useSession();
+  const { status, data: sessionData } = useSession();
 
   return (
     <>
@@ -13,23 +13,26 @@ const Home: NextPage = () => {
         <title>T3 Notes App</title>
       </Head>
       <Layout>
-        {status === "loading" && <h2>Loading...</h2>}
-
-        <Link href={"/login"}>
-          <p className="pb-1 text-2xl text-white underline">
-            {status === "authenticated" ? "Account" : "Log in"}
+        <div className="flex flex-col items-center	">
+          {status === "loading" && <p>Loading...</p>}
+          <p className="pb-4 text-center text-5xl font-extrabold ">
+            Effortlessly create <span className="text-primary">notes</span> and
+            organize them in <span className="text-secondary">folders</span>
           </p>
-        </Link>
-        {status === "authenticated" && (
-          <>
-            <Link href={"/folders"}>
-              <p className="pb-1 text-2xl text-white underline">Your Folders</p>
+          <p className="pb-6 text-center text-xl">
+            Make it easy to keep track of your thoughts, ideas, and projects.
+            Sign up today to streamline your note-taking process.
+          </p>
+          {sessionData ? (
+            <Link href="/folders" className="btn-accent btn">
+              Get Started
             </Link>
-          </>
-        )}
-        {/*  {sessionData?.user !== undefined && (
-          
-        )} */}
+          ) : (
+            <button className="btn-accent btn" onClick={() => void signIn()}>
+              Get Started!
+            </button>
+          )}
+        </div>
       </Layout>
     </>
   );
