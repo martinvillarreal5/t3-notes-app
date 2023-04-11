8a24409d0ba596c3c72218f3dfbde778366fed46
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-11 12:19:30 -0300

    Update folder router:    *Change getById procedure to also return folder's ancestors in array

--- src/server/api/routers/folder.ts
+++ src/server/api/routers/folder.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { arrayToTree } from "performant-array-to-tree";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+//import type { Folder, Note } from "@prisma/client";
 
 export const folderRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
@@ -22,18 +23,57 @@ export const folderRouter = createTRPCRouter({
     }),
   getById: protectedProcedure
     .input(z.object({ folderId: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.folder.findFirstOrThrow({
+    .query(async ({ ctx, input }) => {
+      //TODO make a single $rawQuery using a recursive query
+
+      const folder = await ctx.prisma.folder.findFirstOrThrow({
         where: {
           id: input.folderId,
           userId: ctx.session.user.id,
         },
         include: {
           subFolders: { select: { id: true, title: true } },
-          parentFolder: { select: { id: true, title: true } },
+          parentFolder: {
+            select: { id: true, title: true },
+          },
           notes: true,
         },
       });
+      type FolderWithAncestors = typeof folder & {
+        ancestors: {
+          title: string;
+          id: string;
+        }[];
+      };
+      const folderWithAncestors: FolderWithAncestors = {
+        ...folder,
+        ancestors: [],
+      };
+      const findAncestorAndPushToAncestorsArray = async (id: string) => {
+        const folder = await ctx.prisma.folder.findFirstOrThrow({
+          where: {
+            id: id,
+            userId: ctx.session.user.id,
+          },
+          select: {
+            id: true,
+            title: true,
+            parentFolderId: true,
+          },
+        });
+        folderWithAncestors.ancestors.unshift({
+          id: folder.id,
+          title: folder.title,
+        });
+        if (folder.parentFolderId) {
+          await findAncestorAndPushToAncestorsArray(folder.parentFolderId);
+        }
+      };
+
+      if (folder.parentFolderId) {
+        await findAncestorAndPushToAncestorsArray(folder.parentFolderId);
+      }
+      return folderWithAncestors;
     }),
   getFoldersTree: protectedProcedure.query(async ({ ctx }) => {
     const flatArray = await ctx.prisma.folder.findMany({
