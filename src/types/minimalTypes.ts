426da48dfe35ad09e9e691bfd56a8f00cfe624ee
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-11 20:27:15 -0300

    Create minimalTypes file

---
+++ src/types/minimalTypes.ts
@@ -0,0 +1,13 @@
+export type FolderMinimalInfo = {
+  id: string;
+  title: string;
+};
+
+/* export type NoteMinimalInfo = {
+  id: string;
+  title: string | null;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+ */
