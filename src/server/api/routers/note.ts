1f4814eb7dd67770d1e10107e87707c8babfb8a6
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-11 17:52:17 -0300

    Update Note router: combine create methods in a general one

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -35,32 +35,26 @@ export const noteRouter = createTRPCRouter({
       z.object({
         content: z.string(),
         title: z.string().optional(),
+        folderId: z.string().nullable(),
       })
     )
     .mutation(({ ctx, input }) => {
+      const createFields = {
+        content: input.content,
+        title: input.title,
+        user: { connect: { id: ctx.session.user.id } },
+      };
+      if (input.folderId) {
+        return ctx.prisma.note.create({
+          data: {
+            ...createFields,
+            folder: { connect: { id: input.folderId } },
+          },
+        });
+      }
       return ctx.prisma.note.create({
         data: {
-          content: input.content,
-          title: input.title,
-          user: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
-  createFolderNote: protectedProcedure
-    .input(
-      z.object({
-        content: z.string(),
-        title: z.string().optional(),
-        folderId: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.note.create({
-        data: {
-          content: input.content,
-          title: input.title,
-          user: { connect: { id: ctx.session.user.id } },
-          folder: { connect: { id: input.folderId } },
+          ...createFields,
         },
       });
     }),
