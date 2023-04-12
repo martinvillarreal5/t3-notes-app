3c16cb6c77b9c60906d84a7e027c82c705cff1c9
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-12 16:27:50 -0300

    Refactor createFolderModal to use useForm and zodResolver.

--- src/components/folders/createFolderModal.tsx
+++ src/components/folders/createFolderModal.tsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import { api } from "~/utils/api";
 import Modal from "../ui/headlessUIModal";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
 type ModalProps = {
   isOpen: boolean;
   setIsOpen: (value: React.SetStateAction<boolean>) => void;
@@ -12,6 +16,15 @@ type ModalProps = {
   folders?: FolderMinimalInfo[] | Folder[]; //Find a way to make this prop required when parentId is not specified as prop
 };
 
+const createFolderFormSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Folder title is required." })
+    .max(30, { message: "Folder title must 30 characters long or less." }),
+});
+
+type FormData = z.infer<typeof createFolderFormSchema>;
+
 const CreateFolderModal = ({
   isOpen,
   setIsOpen,
@@ -19,14 +32,20 @@ const CreateFolderModal = ({
   parentId = null,
   folders,
 }: ModalProps) => {
-  const [folderTitle, setFolderTitle] = useState("");
   const [Error, setError] = useState("");
-
+  const {
+    register,
+    handleSubmit,
+    reset: resetForm,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(createFolderFormSchema),
+  });
   const ctx = api.useContext();
 
   const createFolder = api.folder.create.useMutation({
     onError(error) {
-      void setError(error.message);
+      void setError(error.message); //TODO Replace with toast indicating server error
       console.log("Error creating folder: " + error.message);
     },
     onSuccess: () => {
@@ -35,17 +54,15 @@ const CreateFolderModal = ({
         : void ctx.folder.getRootFolders.invalidate();
       void ctx.folder.getFoldersTree.invalidate();
       void setError("");
-      void setFolderTitle(""); //? Necesary?
-      void setIsOpen(false);
+      void resetForm(); //TODO move to onSubmit since input is now validated?
+      void setIsOpen(false); //TODO move to onSubmit since input is now validated?
     },
   });
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
+  const onSubmit = (data: FormData) => {
     if (!parentId) {
       // If the folder is a root level folder (has no parent)
-      if (folders?.some((folder) => folder.title === folderTitle)) {
+      if (folders?.some((folder) => folder.title === data.title)) {
         //? the unique compuound constraint of the prisma model wont work because i doesnt that field
         //? with null values as equal. So even if they have the same title, the parentId is null and
         //? prisma will take it as diferent values
@@ -59,7 +76,7 @@ const CreateFolderModal = ({
     }
 
     createFolder.mutate({
-      title: folderTitle,
+      title: data.title,
       parentFolderId: parentId,
     });
   };
@@ -75,25 +92,30 @@ const CreateFolderModal = ({
         // `Add a new folder in the 'Folders' directory to your account.`
       }
     >
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          name="folderTitle"
           placeholder="Folder Title"
           className="input-bordered input input-sm sm:input-md mt-2.5 w-full"
           autoComplete="off"
-          value={folderTitle}
-          onChange={(e) => setFolderTitle(e.target.value)}
+          //TODO Invalidate on mutation
+          {...register("title")}
         />
         {Error && <p className="text-error my-2">{Error}</p>}
+        {errors.title && errors.title.message}
         <div className="modal-action ">
-          <button type="submit" className="btn-success btn">
+          <button
+            type="submit"
+            className="btn-success btn"
+            //TODO Invalidate on mutation
+          >
             Save
           </button>
           <button
             type="button"
             className="btn-error btn"
             onClick={() => setIsOpen(false)}
+            //TODO Invalidate on mutation???
           >
             Cancel
           </button>
