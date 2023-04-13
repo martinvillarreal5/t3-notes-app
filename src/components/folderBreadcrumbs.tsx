7599f293075f16245a25094a01e30ffd1ecdf472
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-13 18:55:11 -0300

    Update folderBreadcrumbs.tsx

--- src/components/folderBreadcrumbs.tsx
+++ src/components/folderBreadcrumbs.tsx
@@ -1,10 +1,7 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
-import {
-  Home as HomeIcon,
-  ChevronRight as ChevronRightIcon,
-} from "lucide-react";
+import { ChevronRight as ChevronRightIcon } from "lucide-react";
 
 const FolderBreadcrumbs = ({ folderId }: { folderId: string }) => {
   const { data: sessionData } = useSession();
@@ -22,26 +19,31 @@ const FolderBreadcrumbs = ({ folderId }: { folderId: string }) => {
 
   return (
     <>
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row flex-wrap items-center gap-2">
         {status === "success" && (
           <>
-            <HomeIcon />
-            <Link href="/folders" className="pl-2 text-lg">
+            <Link href="/folders" className="text-lg">
               Folders
             </Link>
             {folder.ancestors.map((ancestor) => {
               return (
-                <div key={ancestor.id} className="flex flex-row items-center">
-                  <ChevronRightIcon className="pl-2" />
-                  <Link
-                    href={`/folders/${ancestor.id}`}
-                    className="pl-2 text-lg"
-                  >
+                <div
+                  key={ancestor.id}
+                  className="flex flex-row items-center gap-2"
+                >
+                  <ChevronRightIcon size={16} />
+                  <Link href={`/folders/${ancestor.id}`} className="text-lg">
                     {ancestor.title}
                   </Link>
                 </div>
               );
             })}
+            <div key={folder.id} className="flex flex-row items-center gap-2">
+              <ChevronRightIcon size={16} />
+              <p className="text-lg underline underline-offset-4	">
+                {folder.title}
+              </p>
+            </div>
           </>
         )}
       </div>
