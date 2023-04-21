db64e329661ce4d08656f049486f550fb1361bdf
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-21 18:04:49 -0300

    Update createFolderModal:    *Update create folder schema    *Improve  UX

--- src/components/folders/createFolderModal.tsx
+++ src/components/folders/createFolderModal.tsx
@@ -20,7 +20,7 @@ const createFolderFormSchema = z.object({
   title: z
     .string()
     .min(1, { message: "Folder title is required." })
-    .max(30, { message: "Folder title must 30 characters long or less." }),
+    .max(30, { message: "Folder title must be 30 characters long or less." }),
 });
 
 type FormData = z.infer<typeof createFolderFormSchema>;
@@ -33,6 +33,7 @@ const CreateFolderModal = ({
   folders,
 }: ModalProps) => {
   const [Error, setError] = useState("");
+  const [isMutating, setIsMutating] = useState(false);
   const {
     register,
     handleSubmit,
@@ -44,18 +45,26 @@ const CreateFolderModal = ({
   const ctx = api.useContext();
 
   const createFolder = api.folder.create.useMutation({
-    onError(error) {
-      void setError(error.message); //TODO Replace with toast indicating server error
+    onMutate: () => {
+      void setIsMutating(true);
+    },
+    onError: (error) => {
+      void setError(error.message); //TODO Replace with toast indicating server error?
+      void setIsMutating(false);
       console.log("Error creating folder: " + error.message);
     },
     onSuccess: () => {
+      void setError("");
       parentId
         ? void ctx.folder.getById.invalidate({ folderId: parentId })
         : void ctx.folder.getRootFolders.invalidate();
       void ctx.folder.getFoldersTree.invalidate();
-      void setError("");
-      void resetForm(); //TODO move to onSubmit since input is now validated?
-      void setIsOpen(false); //TODO move to onSubmit since input is now validated?
+
+      void setIsOpen(false);
+      void setTimeout(() => {
+        resetForm();
+        setIsMutating(false);
+      }, 200);
     },
   });
 
@@ -63,11 +72,11 @@ const CreateFolderModal = ({
     if (!parentId) {
       // If the folder is a root level folder (has no parent)
       if (folders?.some((folder) => folder.title === data.title)) {
-        //? the unique compuound constraint of the prisma model wont work because i doesnt that field
-        //? with null values as equal. So even if they have the same title, the parentId is null and
-        //? prisma will take it as diferent values
-        //? https://www.prisma.io/docs/reference/api-reference/prisma-schema-reference#unique-1
-        //? https://github.com/prisma/prisma/issues/3197console.log("Repeated folder title");
+        // the unique compound constraint of the prisma model wont work because i doesnt that field
+        // with null values as equal. So even if they have the same title, the parentId is null and
+        // prisma will take it as diferent values
+        // https://www.prisma.io/docs/reference/api-reference/prisma-schema-reference#unique-1
+        // https://github.com/prisma/prisma/issues/3197console.log("Repeated folder title");
         setError(
           "Folder title is already used by another folder in this directory"
         );
@@ -89,7 +98,6 @@ const CreateFolderModal = ({
         parentTitle
           ? `Add a new sub-folder in the '${parentTitle}' directory to your account.`
           : `Add a new folder to your account.`
-        // `Add a new folder in the 'Folders' directory to your account.`
       }
     >
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -98,7 +106,7 @@ const CreateFolderModal = ({
           placeholder="Folder Title"
           className="input-bordered input input-sm sm:input-md mt-2.5 w-full"
           autoComplete="off"
-          //TODO Invalidate on mutation
+          disabled={isMutating}
           {...register("title")}
         />
         {Error && <p className="text-error mt-2">{Error}</p>}
@@ -109,7 +117,7 @@ const CreateFolderModal = ({
           <button
             type="submit"
             className="btn-success btn"
-            //TODO Invalidate on mutation
+            disabled={isMutating}
           >
             Save
           </button>
@@ -117,7 +125,7 @@ const CreateFolderModal = ({
             type="button"
             className="btn-error btn"
             onClick={() => setIsOpen(false)}
-            //TODO Invalidate on mutation???
+            disabled={isMutating}
           >
             Cancel
           </button>
