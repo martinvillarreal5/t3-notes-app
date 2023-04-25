cf4985d0e10d800a0f3b92847a86dc7860c5c4bf
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-24 21:53:00 -0300

    update cnHelper:    *remove rule comments    *remove return type definition

--- src/utils/cnHelper.tsx
+++ src/utils/cnHelper.tsx
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]): string {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
