2533694c59259f3e8e3beca605bd3dc0cd4e815e
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-26 12:22:49 -0300

    update drawer:    *make drawer composable    *add orientation prop    *update style

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,23 +1,110 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { X as CloseIcon } from "lucide-react";
-type ModalProps = {
+import { cn } from "~/utils/cnHelper";
+
+const DrawerDescription = Dialog.Description;
+const DrawerTitle = Dialog.Title;
+
+const DrawerBlackdrop = () => {
+  return (
+    <Transition.Child
+      as={Fragment}
+      enter="ease-in-out duration-500"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="ease-in-out duration-500"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="fixed inset-0 bg-black bg-opacity-60" />
+    </Transition.Child>
+  );
+};
+
+const DrawerPanelTransition = ({
+  children,
+  orientation,
+}: {
+  children: React.ReactNode;
+  orientation: "right" | "left";
+}) => {
+  return (
+    <Transition.Child
+      as={Fragment}
+      enter="transform transition ease-in-out duration-500 sm:duration-700"
+      enterFrom={orientation === "left" ? "translate-x-0" : "translate-x-full"}
+      enterTo={orientation === "left" ? "translate-x-full" : "translate-x-0"}
+      leave="transform transition ease-in-out duration-500 sm:duration-700"
+      leaveFrom={orientation === "left" ? "translate-x-full" : "translate-x-0"}
+      leaveTo={orientation === "left" ? "translate-x-0" : "translate-x-full"}
+    >
+      {children}
+    </Transition.Child>
+  );
+};
+
+const EaseInOutTransition = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Transition.Child
+      as={Fragment}
+      enter="ease-in-out duration-500"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="ease-in-out duration-500"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {children}
+    </Transition.Child>
+  );
+};
+
+const DrawerCloseButton = ({
+  setIsOpen,
+  orientation,
+}: {
+  setIsOpen: (value: React.SetStateAction<boolean>) => void;
+  orientation: "right" | "left";
+}) => {
+  return (
+    <EaseInOutTransition>
+      <div
+        className={cn(
+          orientation === "left"
+            ? "left-full -mr-8 pl-2 sm:-mr-10 sm:pl-4"
+            : "left-0 -ml-8 pr-2 sm:-ml-10  sm:pr-4",
+          "absolute top-0 flex pt-4   "
+        )}
+      >
+        <button
+          type="button"
+          className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+          onClick={() => setIsOpen(false)}
+        >
+          <span className="sr-only">Close panel</span>
+          <CloseIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+    </EaseInOutTransition>
+  );
+};
+
+type DrawerProps = {
   isOpen: boolean;
   setIsOpen: (value: React.SetStateAction<boolean>) => void;
   children?: React.ReactNode;
-  title?: string;
-  description?: string;
   initialFocusRef?: React.MutableRefObject<null>;
+  orientation?: "right" | "left";
 };
 
 const Drawer = ({
   isOpen,
   setIsOpen,
-  title,
-  description,
   children,
   initialFocusRef,
-}: ModalProps) => {
+  orientation = "left",
+}: DrawerProps) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
@@ -26,70 +113,28 @@ const Drawer = ({
         onClose={() => setIsOpen(false)}
         initialFocus={initialFocusRef}
       >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-60" />
-        </Transition.Child>
-
+        <DrawerBlackdrop />
         <div className="fixed inset-x-0 inset-y-0 overflow-hidden">
           <div className="absolute inset-x-0 inset-y-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-full flex max-w-full">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-0"
-                enterTo="translate-x-full"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-full"
-                leaveTo="translate-x-0"
-              >
+            <div
+              className={cn(
+                `pointer-events-none fixed inset-y-0  flex max-w-full`,
+                orientation === "left" ? "right-full pl-10" : "right-0 pl-10"
+              )}
+            >
+              <DrawerPanelTransition orientation={orientation}>
                 <Dialog.Panel className="z-100 pointer-events-auto relative w-screen max-w-md">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-full top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                      <button
-                        type="button"
-                        className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                        onClick={() => setIsOpen(false)}
-                      >
-                        <span className="sr-only">Close panel</span>
-                        <CloseIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
+                  <DrawerCloseButton
+                    orientation={orientation}
+                    setIsOpen={setIsOpen}
+                  />
                   <div className="bg-base-100 flex h-full flex-col overflow-y-auto py-6 shadow-xl">
-                    {title && (
-                      <div className="px-4 sm:px-6">
-                        <Dialog.Title className="text-lg font-medium leading-6">
-                          {title}
-                        </Dialog.Title>
-                      </div>
-                    )}
-                    {description && (
-                      <div className="px-4 sm:px-6">
-                        <Dialog.Description>{description}</Dialog.Description>
-                      </div>
-                    )}
                     <div className="relative mt-6 flex-1 px-4 sm:px-6">
                       {children}
                     </div>
                   </div>
                 </Dialog.Panel>
-              </Transition.Child>
+              </DrawerPanelTransition>
             </div>
           </div>
         </div>
@@ -98,4 +143,4 @@ const Drawer = ({
   );
 };
 
-export default Drawer;
+export { Drawer, DrawerDescription, DrawerTitle };
