452628f874da050a8d10e0873159886ceb799b99
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-04-30 20:43:57 -0300

    feat: add confirmation to deleteFolderButton:    *add modal to confirmate folder deletion    *add folderTitle prop to show on modal    *add disabled prop    *change button to open modal instead of deleting folder on click     *change button's disabled prop to also check for disabled prop    *change button's disabled prop to also check for mutation success    *revert mutation's onSuccess to redirect to folders/folder

--- src/components/folders/deleteFolderButton.tsx
+++ src/components/folders/deleteFolderButton.tsx
@@ -1,16 +1,23 @@
 import { Trash2 as TrashIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
+import { Modal, ModalPanel, ModalTitle, ModalDescription } from "../ui/modal";
+import { useState } from "react";
 
 type DeleteFolderButton = {
   folderId: string;
+  folderTitle: string;
   parentFolderId?: string | null;
+  disabled?: boolean;
 };
 
 const DeleteFolderButton = ({
   folderId,
   parentFolderId = null,
+  folderTitle,
+  disabled = false,
 }: DeleteFolderButton) => {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const ctx = api.useContext();
   const deleteFolder = api.folder.delete.useMutation({
@@ -18,31 +25,57 @@ const DeleteFolderButton = ({
       void ctx.folder.getFoldersTree.invalidate();
       if (parentFolderId) {
         void ctx.folder.getById.invalidate({ folderId: parentFolderId });
-        //void router.push(`/folders/${parentFolderId}`);
-        void router.back();
+        void router.push(`/folders/${parentFolderId}`);
+        //void router.back();
       } else {
         void ctx.folder.getRootFolders.invalidate();
-
-        //void router.push(`/folders/`);
-        void router.back();
+        void router.push(`/folders`);
+        //void router.back();
       }
     },
   });
-  const deleteFolderHandler = (deleteId: string) => {
-    deleteFolder.mutate({
-      id: deleteId,
-    });
-  };
   return (
-    <button
-      title="Delete this Folder"
-      className="btn-square btn-sm btn"
-      onClick={() => deleteFolderHandler(folderId)}
-      disabled={deleteFolder.isLoading}
-      //TODO open confirmation modal
-    >
-      <TrashIcon />
-    </button>
+    <>
+      <button
+        title="Delete this Folder"
+        className="btn-square btn-sm btn"
+        onClick={() => setIsOpen(true)}
+        disabled={disabled || deleteFolder.isLoading || deleteFolder.isSuccess}
+        //TODO open confirmation modal
+      >
+        <TrashIcon />
+      </button>
+      <Modal setIsOpen={setIsOpen} isOpen={isOpen}>
+        <ModalPanel>
+          <ModalTitle className={"pb-1 text-xl"}>
+            Delete {folderTitle}?
+          </ModalTitle>
+          <ModalDescription className={"pb-4 text-base"}>
+            This will permantly remove {folderTitle} from your folders
+          </ModalDescription>
+          <div className="flex flex-row justify-between">
+            <button
+              className="btn btn-error"
+              onClick={() =>
+                deleteFolder.mutate({
+                  id: folderId,
+                })
+              }
+              disabled={deleteFolder.isLoading || deleteFolder.isSuccess}
+            >
+              Delete
+            </button>
+            <button
+              className="btn "
+              onClick={() => setIsOpen(false)}
+              disabled={deleteFolder.isLoading || deleteFolder.isSuccess}
+            >
+              Cancel
+            </button>
+          </div>
+        </ModalPanel>
+      </Modal>
+    </>
   );
 };
 
