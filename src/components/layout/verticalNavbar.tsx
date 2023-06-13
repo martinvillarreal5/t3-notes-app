647fc27a294b05c37419913e353e26bd66517874
Martin Villarreal <martinvictorvillarreal@gmail.com>
2023-06-13 15:20:10 -0300

    update verticalNavbar:   *add current page logic & style

--- src/components/layout/verticalNavbar.tsx
+++ src/components/layout/verticalNavbar.tsx
@@ -1,19 +1,20 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import FolderTree from "../folders/folderTree";
+import { useRouter } from "next/router";
+import { cn } from "~/utils/cnHelper";
 
-const NavbarItem = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
+const NavbarItem = ({ children, href }: { children: string; href: string }) => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
   return (
     <li>
       <Link
-        className="hover:bg-neutral-focus focus:bg-neutral-focus block w-full rounded-md 
-        py-1 pl-1 text-xl"
+        className={cn(
+          currentRoute === href && "bg-neutral-focus",
+          `block w-full rounded-md py-1 pl-1 
+        text-xl hover:bg-neutral-focus focus:bg-neutral-focus`
+        )}
         href={href}
       >
         {children}
